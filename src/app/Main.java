f801e3aba6c392eb718ff35f81eaac15b8a8d54b
Ed Gluschenko <edgluschenko3@gmail.com>
2025-04-21 18:28:49 +0300

    changed output information

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,9 +3,8 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        (Logger.getInstance()).log("start");
-        (Logger.getInstance()).log("first proces");
-        (Logger.getInstance()).log("finish");
+        (Logger.getInstance()).log("log level info: start process");
+        (Logger.getInstance()).log("log level info: finish process");
         System.out.println("got these logs: ");
         Logger.getLogs().forEach(System.out::println);
     }
