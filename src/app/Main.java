ff49e832b0af8ce83c2220365f755b38188e9759
Ed Gluschenko <edgluschenko3@gmail.com>
2025-04-22 08:35:52 +0300

    add new check

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,5 +7,16 @@ public class Main {
         (Logger.getInstance()).log("log level info: finish process");
         System.out.println("got these logs: ");
         Logger.getLogs().forEach(System.out::println);
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        System.out.println("--------------------------------------");
+        System.out.println("checking if it is same instance");
+        // Перевірка, чи це той самий об'єкт
+        if (logger1 == logger2) {
+            System.out.println("These are the same instance.");
+        } else {
+            System.out.println("These are different instances!");
+        }
     }
 }
